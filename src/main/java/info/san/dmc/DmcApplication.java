3583c77cd0b2188608c787f3748b37d5aa61c4ae
ANGELLOZ-NICOUD Sébastien <sebastien.angelloz@gmail.com>
2020-01-17 22:54:19 +0100

    Initialisation du projet.

---
+++ src/main/java/info/san/dmc/DmcApplication.java
@@ -0,0 +1,13 @@
+package info.san.dmc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/info/san/dmc/DmcApplicationTests.java
@@ -0,0 +1,13 @@
+package info.san.dmc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
